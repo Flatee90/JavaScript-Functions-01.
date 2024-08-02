5e08ba35c554b02c91ceee0384cf94abe8999bda
Flatee90 <flatee90@gmail.com>
2024-08-02 23:31:41 +0000

    javscript refrain assignment

---
+++ index.js
@@ -0,0 +1,82 @@
+function refrain() {
+    console.log("You should be here with me");
+    console.log("Giving me what I need");
+    console.log("Won't you come closer");
+    console.log("I can' wait that much longer");
+    console.log("If you could only see");
+    console.log("How you're affecting me");
+    console.log("Wish I was your home");
+    console.log("Wish you were never gone");
+}
+
+function refrain2() {
+    console.log("Why, why?");
+    console.log("Why, why, oh why?");
+    console.log("Why, why?");
+    console.log("Why, oh why?");
+}
+
+function refrain3() {
+    console.log("I'll go the distance for you");
+    console.log("I'll go the distance for you");
+    console.log("I'll go the distance for you");
+    console.log("Hoo");
+}
+
+function refrain4() {
+    console.log("Two days is all I need");
+    console.log("We could make believe");
+    console.log("Time doesn't matter");
+    console.log("Let's just get lost together");
+    console.log("Jump through the fire for me");
+    console.log("Cross all the land and sea");
+    console.log("If you could take it");
+    console.log("Then we could make it");
+}
+
+function refrain5() {
+    console.log("Why, why?");
+    console.log("Why, why, oh why?");
+    console.log("Why, why?");
+    console.log("Why, oh why?");
+}
+
+ function refrain6() {
+    console.log("I'll go the distance for you");
+    console.log("I'll go the distance for you");
+    console.log("I'll go the distance for you");
+    console.log("Hoo");
+    console.log("I'll go the distance for you");
+    console.log("I'll go the distance for you");
+    console.log("I'll go the distance for you");
+    console.log("Hoo");
+}
+
+function refrain7() {
+    console.log("I just gotta reach you, hoo");
+    console.log("I just gotta hold you now, baby");
+    console.log("I just gotta break the wall for you, hoo");
+    console.log("I need you hurry");
+    console.log("Now!");
+}
+
+function refrain8() {
+    console.log("Yeah, for you");
+    console.log("(I'll go the distance for you)");
+    console.log("I'll go, I'll go");
+    console.log("(I'll go the distance for you)");
+    console.log("I'll go the distance for you");
+    console.log("(I'll go the distance for you)");
+    console.log("Yeah, for you");
+}
+
+
+
+
+
+
+
+
+
+
+
